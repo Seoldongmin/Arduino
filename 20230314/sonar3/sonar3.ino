a1e2946887d3796a603e4e5aa8317d3ad2324a13
Seoldongmin <125715732+Seoldongmin@users.noreply.github.com>
2023-04-05 10:53:43 +0900

    Add files via upload

--- 20230314/sonar3/sonar3.ino
+++ 20230314/sonar3/sonar3.ino
@@ -1,13 +1,15 @@
 #define TRIG1 3 // 초음파 센서 1번 Trig 핀 번호
 #define ECHO1 4 // 초음파 센서 1번 Echo 핀 번호
 
-#define TRIG2 5 // 초음파 센서 2번 Trig 핀 번호
-#define ECHO2 6 // 초음파 센서 2번 Echo 핀 번호
+#define TRIG1 5 // 초음파 센서 1번 Trig 핀 번호
+#define ECHO1 6 // 초음파 센서 1번 Echo 핀 번호
 
-#define TRIG3 7 // 초음파 센서 3번 Trig 핀 번호
-#define ECHO3 8 // 초음파 센서 3번 Echo 핀 번호
+#define TRIG1 7 // 초음파 센서 1번 Trig 핀 번호
+#define ECHO1 8 // 초음파 센서 1번 Echo 핀 번호
 
-void setup() {
+
+void setup() 
+{
   // put your setup code here, to run once:
   pinMode(TRIG1, OUTPUT);
   pinMode(ECHO1,  INPUT);
@@ -33,7 +35,8 @@ long sonar1(void) // 초음파 센서 1번 측정 함수
   
 }
 
-long sonar2(void) // 초음파 센서 1번 측정 함수
+
+long sonar1(void) // 초음파 센서 1번 측정 함수
 {
   long duration, distance;
   digitalWrite(TRIG2, HIGH);
@@ -45,21 +48,33 @@ long sonar2(void) // 초음파 센서 1번 측정 함수
   
 }
 
+
+
+long sonar1(void) // 초음파 센서 1번 측정 함수
+{
+  long duration, distance;
+  digitalWrite(TRIG3, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG3, LOW);
+  duration = pulseIn(ECHO3, HIGH);
+  distance = ( (float)(340*duration)/1000)/2;
+  return distance;
+  
+}
+
+
 void loop()
 {
   // put your main code here, to run repeatedly:
-  long duration, distance;
-  digitalWrite(TRIG1, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG1, LOW);
-  duration = pulseIn(ECHO1, HIGH);
-  distance = ( (float)(340*duration)/1000)/2;
-  //Serial.print("Duration: ");
-  //Serial.println(duration);
-  Serial.print("Distance1:");
-  Serial.println(sonar1());
-  Serial.print("Distance2:");
-  Serial.println(sonar2());
+  
+  //Serial.print("Duration:");
+  //Serial.println(duratoin);
+  Serial.print("Distance:");
+  Serial.println( sonar1 ());
+  Serial.print("Distance:");
+  Serial.println( sonar2 ());
+  Serial.print("Distance:");
+  Serial.println( sonar3 ());
   delay(500);
 
 }
